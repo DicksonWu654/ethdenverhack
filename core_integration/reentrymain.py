8cce24b59e3b98fde752eeab13b15a389b769382
Dickson Wu <33645481+DicksonWu654@users.noreply.github.com>
2022-02-19 17:32:45 -0700

    modified the thing to fit with frontend

--- core_integration/reentrymain.py
+++ core_integration/reentrymain.py
@@ -1,34 +1,38 @@
 import openai
 import dataset_loader as loader
 from reentrysamples import samples
-from reentrytest import test
 
-prompt = samples + "\n --SmartContract-- \n" + test + "\n--Classification--"
 
-# initialize the openAI API
-KEY = 'key.txt'
-key = loader.load_key(KEY)
-openai.api_key = key
+def process_gpt3(test):
+    prompt = samples + "\n --SmartContract-- \n" + test + "\n--Classification--"
 
-tries = 0
-while True:
-    try:
-        result = openai.Completion.create(
-            engine="text-ada-001",
-            prompt=prompt,
-            max_tokens=5
-        )
-        vulnerability = result["choices"][0]["text"]
-        break
-    except Exception:
-        print(Exception)
-        tries += 1
-        if tries == 3:
+    # initialize the openAI API
+    KEY = 'key.txt'
+    key = loader.load_key(KEY)
+    openai.api_key = key
+
+    tries = 0
+    while True:
+        try:
+            result = openai.Completion.create(
+                engine="text-ada-001",
+                prompt=prompt,
+                max_tokens=5
+            )
+            vulnerability = result["choices"][0]["text"]
             break
+        except Exception:
+            print(Exception)
+            tries += 1
+            if tries == 3:
+                break
 
-if "Yes" in vulnerability:
-    print("Yes")
-elif "No" in vulnerability:
-    print("No")
-else:
-    print("Error", result)
+    if "Yes" in vulnerability:
+        print("Yes")
+        return "Yes"
+    elif "No" in vulnerability:
+        print("No")
+        return "No"
+    else:
+        print("Error", result)
+        return "Error"
