a3fa96223fe3ff056398d9c1516152716144d0a0
Robert MacWha - Laptop <trebor.ahwcam@gmail.com>
2022-03-02 11:42:03 -0500

    Update dataset

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ from v2_backend.dataset_processor import generate_dataset
 from v2_backend.model import Model
 
 # generate the dataset
-#generate_dataset('dataset', 10)
+generate_dataset('dataset', 50)
 
 # build the model
 m = Model(padding=256)
 m.build_model()
 print(m.model.summary())
 
-m.train('dataset/train.pkl', 3)
+m.train('dataset/train.pkl', 10)
 m.save_model('models/v1')
